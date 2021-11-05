58d36dda2a8d5cb098aa1c33781cfc88979fa14c
suvigyabhardwaj <suvigya0797@gmail.com>
2021-11-05 17:11:25 +0530

    added form action url

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/result')
+@app.route('/result', methods=["POST"])
 def result():
-    return render_template('result.html')
+    if request.method=="POST":
+        return render_template('result.html')
+
+
 
 if __name__ == '__main__':
    app.run(debug=True)
