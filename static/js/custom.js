0445b0c22da148337b6c33bb22bc4704fb5196e3
satyam-seth <satyam1998.1998@gmail.com>
2021-11-25 01:43:46 +0530

    add missing js

---
+++ static/js/custom.js
@@ -0,0 +1,6 @@
+const btn=document.getElementById("btnCopy");
+btn.addEventListener("click",()=>{
+    const copyText = document.getElementById("result");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+});
