d9d6a3012e96df689eed56c59c03ccac65f71c2a
satyam-seth <satyam1998.1998@gmail.com>
2022-03-16 09:35:08 -0700

    add eslint and formatter

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,47 @@
-const gulp = require("gulp");
+const gulp = require('gulp');
 // const uglify = require('gulp-uglify');
-const ts = require("gulp-typescript");
+const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
-const tsProject = ts.createProject("tsconfig.json");
+const tsProject = ts.createProject('tsconfig.json');
 
 // const browserify = require("browserify");
 // const source = require("vinyl-source-stream");
 // const tsify = require("tsify");
 
-
 /**
  * Transpile typescript to javascript
  * @param cb: gulp task callback
  */
- function buildTypescript(cb) {
-    //  TODO: Add uglify and prevent to generate d.ts
-//  https://www.typescriptlang.org/docs/handbook/gulp.html#browserify
-//   const result = browserify({
-//     basedir: ".",
-//     debug: true,
-//     entries: ["./ts/demo.ts"],
-//     cache: {},
-//     packageCache: {},
-//   })
-//     .plugin(tsify)
-//     .bundle()
-//     .pipe(source("index.js"))
-//     .pipe(gulp.dest("./static/js"));
+function buildTypescript(cb) {
+  //  TODO: Add uglify and prevent to generate d.ts
+  //  https://www.typescriptlang.org/docs/handbook/gulp.html#browserify
+  //   const result = browserify({
+  //     basedir: ".",
+  //     debug: true,
+  //     entries: ["./ts/demo.ts"],
+  //     cache: {},
+  //     packageCache: {},
+  //   })
+  //     .plugin(tsify)
+  //     .bundle()
+  //     .pipe(source("index.js"))
+  //     .pipe(gulp.dest("./static/js"));
   const result = gulp
-  .src('./ts/**/*.ts')
-  .pipe(sourcemaps.init())
-  .pipe(tsProject())
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('./static/js'));
+    .src('./ts/**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./static/js'));
   cb();
   return result;
 }
-
-
 
 /**
  * Transpile scss to css
  * @param  cb gulp callback
  */
- function buildScss(cb) {
+function buildScss(cb) {
   const result = gulp
     .src('./scss/**/*.scss')
     .pipe(sourcemaps.init())
@@ -55,11 +52,9 @@ const tsProject = ts.createProject("tsconfig.json");
   return result;
 }
 
+gulp.task('default', gulp.parallel(buildTypescript, buildScss));
 
-gulp.task('default', gulp.parallel( buildTypescript, buildScss),
-);
-
-gulp.task('watch', function(){
-  gulp.watch('./ts/**/*.ts',buildTypescript);
-  gulp.watch('./scss/**/*.scss',buildScss);
+gulp.task('watch', () => {
+  gulp.watch('./ts/**/*.ts', buildTypescript);
+  gulp.watch('./scss/**/*.scss', buildScss);
 });
